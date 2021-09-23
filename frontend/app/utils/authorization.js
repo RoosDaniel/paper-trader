87f6540215ac142d4ba753dff70c529a6210b3ed
Daniel Roos <daniel.roos@cygni.se>
2021-09-23 10:28:01 +0200

    Token expiration is now static instead of relative

--- frontend/app/utils/authorization.js
+++ frontend/app/utils/authorization.js
@@ -3,10 +3,15 @@ const { localStorage } = window;
 
 export const setJWT = response => {
     localStorage.setItem('JWT', response.token);
-    localStorage.setItem('JWTExpiration', response.tokenExpiration);
+
+    const JWTExpiration = new Date(Number(new Date())+response.tokenExpiration*1000);
+
+    localStorage.setItem('JWTExpiration', Number(JWTExpiration));
 };
 
 export const getJWT = () => localStorage.getItem('JWT');
+
+export const getJWTExpiration = () => new Date(localStorage.getItem('JWTExpiration'));
 
 export const removeJWT = () => {
     localStorage.removeItem('JWT');
